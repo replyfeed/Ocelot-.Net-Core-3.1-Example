04940984be266f5c0b87bb9b93091f5c3f5cbabd
replyfeed <46439770+replyfeed@users.noreply.github.com>
2020-07-28 16:53:43 +0300

    Add files via upload
    CustomerManagementApi

---
+++ CustomerManagementApi/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManagementApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        //firmaları getir
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "FirmaA", "FirmaB", "FirmaC", "FirmaD", "FirmaE", };
+        }
+    }
+}
