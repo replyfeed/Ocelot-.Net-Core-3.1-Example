f1f6d44a08a904cefb4ce0351407df51db401d03
replyfeed <46439770+replyfeed@users.noreply.github.com>
2020-07-28 16:57:24 +0300

    Add files via upload

---
+++ MarketingManagementApi/Controllers/MarketingExpertController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketingManagementApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MarketingExpertController : Controller
+    {
+        //pazarlama uzmlanlarını getir
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Uzman1", "Uzman2", "Uzman3", "Uzman4", "Uzman5", };
+        }
+    }
+}
